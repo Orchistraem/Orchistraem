1e299b1f6157f5652f32c11b8c782aa7c2e932ac
zCaps <gabriel.abrantes-alfredo@etu.univ-smb.fr>
2023-12-11 01:38:43 +0100

    Ajout de l'audiogramme vide

---
+++ src/audiogram.ts
@@ -0,0 +1,64 @@
+declare var Chart: any; // Déclare Chart.js comme une variable globale
+
+window.onload = function () {
+    const canvas = document.getElementById('audiogram') as HTMLCanvasElement;
+    if (!canvas) {
+        console.error('Élément canvas non trouvé dans le DOM');
+        return;
+    }
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+        console.error('Impossible de récupérer le contexte de rendu 2D');
+        return;
+    }
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [125, 250, 500, '1k', '2k', '4k', '8k'],
+            datasets: [{
+                label: 'Hearing Level (dB)',
+                data: Array(7).fill(null),
+                backgroundColor: 'rgba(0, 123, 255, 0.2)',
+                borderColor: 'rgba(0, 123, 255, 1)',
+                borderWidth: 1,
+                pointRadius: 0
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false,
+                    reverse: true,
+                    min: -10,
+                    max: 120,
+                    ticks: {
+                        stepSize: 10
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Fréquence (Hz)'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            elements: {
+                line: {
+                    tension: 0
+                }
+            },
+            options: {
+              responsive: false, // Empêche le redimensionnement automatique
+              maintainAspectRatio: true, // Maintient l'aspect ratio si nécessaire
+              // Autres options...
+          },
+        }
+    });
+};
