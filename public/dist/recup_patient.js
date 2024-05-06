059bb4438c6f571ce092e56e06b70368695650a8
Jules <julesche08@gmail.com>
2024-05-06 20:57:15 +0200

    com + fix

--- public/dist/recup_patient.js
+++ public/dist/recup_patient.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+/**
+ * Récupère les informations de tous les patients stockées dans des fichiers JSON spécifiques à chaque dossier de patient.
+ *
+ * Cette fonction itère sur les dossiers de patients stockés dans un répertoire spécifié, extrait les informations
+ * de chaque patient à partir d'un fichier JSON et retourne un tableau contenant ces informations. Chaque entrée
+ * du tableau représente un patient et inclut son identifiant (basé sur le nom du dossier) et son nom (extrait du fichier JSON).
+ * En cas d'erreur lors de la récupération des fichiers ou des données, une erreur est enregistrée et un tableau vide est retourné.
+ *
+ * @returns Une promesse qui résout en un tableau d'objets `PatientInfo`, où chaque objet contient l'identifiant et le nom d'un patient.
+ *          Si une erreur se produit, retourne un tableau vide.
+ */
 function fetchAllPatientInfo() {
     return __awaiter(this, void 0, void 0, function* () {
         const patientsDir = './data/patients';
@@ -34,6 +45,17 @@ function fetchAllPatientInfo() {
         }
     });
 }
+/**
+ * Récupère les noms des dossiers des patients à partir d'un répertoire spécifié.
+ *
+ * Cette fonction envoie une requête HTTP pour accéder au contenu d'un répertoire spécifié et récupérer les noms des dossiers
+ * qu'il contient. Ces noms sont supposés représenter des patients individuels. En cas de succès, elle retourne un tableau de chaînes
+ * avec les noms des dossiers. Si la requête échoue ou si une erreur survient pendant le processus, elle retourne un tableau vide
+ * et enregistre une erreur dans la console.
+ *
+ * @param patientsDir - Le chemin du répertoire contenant les dossiers des patients.
+ * @returns Une promesse qui résout en un tableau de chaînes contenant les noms des dossiers des patients, ou un tableau vide en cas d'erreur.
+ */
 function fetchPatientFolders(patientsDir) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
